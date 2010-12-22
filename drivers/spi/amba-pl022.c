015a0108f545d8ccd0b961bfcf56a3a1bcdd502d
Virupax Sadashivpetimath <virupax.sadashivpetimath@stericsson.com>
2010-12-22 14:52:44 +0530

    amba-pl022: Clear SSE once the transfer is completed
    Clear SSE after the xfer completion and remove the not needed
    default register restoration in suspend.
    
    ST-Ericsson ID: ER 316546
    
    Change-Id: I73368e18bd55abdff6b1909239bcf5b64583b452
    Signed-off-by: Virupax Sadashivpetimath <virupax.sadashivpetimath@stericsson.com>

--- drivers/spi/amba-pl022.c
+++ drivers/spi/amba-pl022.c
@@ -516,7 +516,12 @@ static void giveback(struct pl022 *pl022)
 	msg->state = NULL;
 	if (msg->complete)
 		msg->complete(msg->context);
-	/* This message is completed, so let's turn off the clocks & power */
+
+	/* disable the SPI/SSP operation */
+	writew((readw(SSP_CR1(pl022->virtbase)) &
+		(~SSP_CR1_MASK_SSE)), SSP_CR1(pl022->virtbase));
+
+	/* This message is completed, so let's turn off the clock! */
 	clk_disable(pl022->clk);
 	amba_pclk_disable(pl022->adev);
 	amba_vcore_disable(pl022->adev);
@@ -2228,11 +2233,6 @@ static int pl022_suspend(struct amba_device *adev, pm_message_t state)
 		return status;
 	}
 
-	amba_vcore_enable(adev);
-	amba_pclk_enable(adev);
-	load_ssp_default_config(pl022);
-	amba_pclk_disable(adev);
-	amba_vcore_disable(adev);
 	dev_dbg(&adev->dev, "suspended\n");
 	return 0;
 }
