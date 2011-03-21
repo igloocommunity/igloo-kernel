342d9be2c317d1b0ba62d741d6600a78c3b6254c
Philippe Langlais <philippe.langlais@linaro.org>
2011-03-21 13:26:43 +0100

    mach-ux500: platform data for HED54XXU11 Hall effect sensor
    Hall effect sensor is managed as an input event (SW_LID),
    same management as proximity sensor
    
    Signed-off-by: Philippe Langlais <philippe.langlais@linaro.org>

--- arch/arm/mach-ux500/board-mop500.c
+++ arch/arm/mach-ux500/board-mop500.c
@@ -250,18 +250,25 @@ static struct gpio_keys_button mop500_gpio_keys[] = {
 		.code			= SW_FRONT_PROXIMITY,
 		.active_low		= 0,
 		.can_disable		= 1,
+	},
+	{
+		.desc			= "HED54XXU11 Hall Effect Sensor",
+		.type			= EV_SW,
+		.code			= SW_LID, /* FIXME arbitrary usage */
+		.active_low		= 0,
+		.can_disable		= 1,
 	}
 };
 
-static struct regulator *prox_regulator;
-static int mop500_prox_activate(struct device *dev);
-static void mop500_prox_deactivate(struct device *dev);
+static struct regulator *sensors1p_regulator;
+static int mop500_sensors1p_activate(struct device *dev);
+static void mop500_sensors1p_deactivate(struct device *dev);
 
 static struct gpio_keys_platform_data mop500_gpio_keys_data = {
 	.buttons	= mop500_gpio_keys,
 	.nbuttons	= ARRAY_SIZE(mop500_gpio_keys),
-	.enable		= mop500_prox_activate,
-	.disable	= mop500_prox_deactivate,
+	.enable		= mop500_sensors1p_activate,
+	.disable	= mop500_sensors1p_deactivate,
 };
 
 static struct platform_device mop500_gpio_keys_device = {
@@ -272,23 +279,22 @@ static struct platform_device mop500_gpio_keys_device = {
 	},
 };
 
-static int mop500_prox_activate(struct device *dev)
+static int mop500_sensors1p_activate(struct device *dev)
 {
-	prox_regulator = regulator_get(&mop500_gpio_keys_device.dev,
+	sensors1p_regulator = regulator_get(&mop500_gpio_keys_device.dev,
 						"vcc");
-	if (IS_ERR(prox_regulator)) {
-		dev_err(&mop500_gpio_keys_device.dev,
-			"no regulator\n");
-		return PTR_ERR(prox_regulator);
+	if (IS_ERR(sensors1p_regulator)) {
+		dev_err(&mop500_gpio_keys_device.dev, "no regulator\n");
+		return PTR_ERR(sensors1p_regulator);
 	}
-	regulator_enable(prox_regulator);
+	regulator_enable(sensors1p_regulator);
 	return 0;
 }
 
-static void mop500_prox_deactivate(struct device *dev)
+static void mop500_sensors1p_deactivate(struct device *dev)
 {
-	regulator_disable(prox_regulator);
-	regulator_put(prox_regulator);
+	regulator_disable(sensors1p_regulator);
+	regulator_put(sensors1p_regulator);
 }
 
 /* add any platform devices here - TODO */
@@ -433,10 +439,14 @@ static void __init mop500_init_machine(void)
 	 * all these GPIO pins to the internal GPIO controller
 	 * instead.
 	 */
-	if (machine_is_hrefv60())
+	if (machine_is_hrefv60()) {
 		mop500_gpio_keys[0].gpio = HREFV60_PROX_SENSE_GPIO;
-	else
+		mop500_gpio_keys[1].gpio = HREFV60_HAL_SW_GPIO;
+	}
+	else {
 		mop500_gpio_keys[0].gpio = GPIO_PROX_SENSOR;
+		mop500_gpio_keys[1].gpio = GPIO_HAL_SENSOR;
+	}
 
 	u8500_init_devices();
 
