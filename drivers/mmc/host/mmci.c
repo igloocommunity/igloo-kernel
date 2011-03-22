178133e8dcd2f718c832932dda166261d1ee0d53
Philippe Langlais <philippe.langlais@linaro.org>
2011-03-22 11:36:59 +0100

    MMCI: Add support for non-power-of-two block sizes
    Adds support for any block size over SDIO.
    
    ST-Ericsson ID: AP272999
    
    Change-Id: I5a2654360a2d38e6ab929b099c2bc04668ae9e85
    Signed-off-by: Stefan Nilsson XK <stefan.xk.nilsson@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/15091
    Reviewed-by: Jonas ABERG <jonas.aberg@stericsson.com>

--- drivers/mmc/host/mmci.c
+++ drivers/mmc/host/mmci.c
@@ -52,6 +52,8 @@ static unsigned int fmax = 515633;
  * @sdio: variant supports SDIO
  * @st_clkdiv: true if using a ST-specific clock divider algorithm
  * @pwrreg_powerup: power up value for MMCIPOWER register
+ * @non_power_of_2_blksize: variant supports block sizes that are not
+ *		a power of two.
  */
 struct variant_data {
 	unsigned int		clkreg;
@@ -62,6 +64,7 @@ struct variant_data {
 	bool			sdio;
 	bool			st_clkdiv;
 	unsigned int		pwrreg_powerup;
+	bool			non_power_of_2_blksize;
 };
 
 static struct variant_data variant_arm = {
@@ -89,6 +92,7 @@ static struct variant_data variant_ux500 = {
 	.sdio			= true,
 	.st_clkdiv		= true,
 	.pwrreg_powerup		= MCI_PWR_ON,
+	.non_power_of_2_blksize	= true,
 };
 
 /*
@@ -491,7 +495,6 @@ static void mmci_start_data(struct mmci_host *host, struct mmc_data *data)
 	writel(host->size, base + MMCIDATALENGTH);
 
 	blksz_bits = ffs(data->blksz) - 1;
-	BUG_ON(1 << blksz_bits != data->blksz);
 
 	datactrl = MCI_DPSM_ENABLE | blksz_bits << 4;
 
@@ -951,11 +954,18 @@ static irqreturn_t mmci_irq(int irq, void *dev_id)
 static void mmci_request(struct mmc_host *mmc, struct mmc_request *mrq)
 {
 	struct mmci_host *host = mmc_priv(mmc);
+	struct variant_data *variant = host->variant;
 	unsigned long flags;
 
 	WARN_ON(host->mrq != NULL);
 
-	if (mrq->data && !is_power_of_2(mrq->data->blksz)) {
+	if (mrq->data &&
+	    (!variant->non_power_of_2_blksize ||
+#ifdef CONFIG_ARCH_U8500
+	     !cpu_is_u8500v2() ||
+#endif
+	    (mmc->card && mmc_card_ddr_mode(mmc->card))) &&
+	    !is_power_of_2(mrq->data->blksz)) {
 		dev_err(mmc_dev(mmc), "unsupported block size (%d bytes)\n",
 			mrq->data->blksz);
 		mrq->cmd->error = -EINVAL;
