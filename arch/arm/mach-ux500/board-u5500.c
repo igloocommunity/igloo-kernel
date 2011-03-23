c88b2275caa2f3cbbee5154d9e5c645a0615e975
Philippe Langlais <philippe.langlais@linaro.org>
2011-03-23 09:15:40 +0100

    u5500: add DB5500 keypad driver
    ST-Ericsson ID: AP273221
    
    Change-Id: I0f096b8364797595084a84cb61bf13de61a3c0a2
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/5408
    Tested-by: Rabin VINCENT <rabin.vincent@stericsson.com>
    Tested-by:  build servers
    Reviewed-by: Linus WALLEIJ <linus.walleij@stericsson.com>

--- arch/arm/mach-ux500/board-u5500.c
+++ arch/arm/mach-ux500/board-u5500.c
@@ -10,6 +10,7 @@
 #include <linux/amba/bus.h>
 #include <linux/gpio.h>
 #include <linux/irq.h>
+#include <linux/input/matrix_keypad.h>
 
 #include <asm/mach/arch.h>
 #include <asm/mach-types.h>
@@ -17,8 +18,36 @@
 #include <mach/hardware.h>
 #include <mach/devices.h>
 #include <mach/setup.h>
+#include <mach/db5500-keypad.h>
 
 #include "devices-db5500.h"
+
+/*
+ * Keypad
+ */
+
+static const unsigned int u5500_keymap[] = {
+	KEY(4, 0, KEY_CAMERA), /* Camera2 */
+	KEY(4, 1, KEY_CAMERA_FOCUS), /* Camera1 */
+	KEY(4, 2, KEY_MENU),
+	KEY(4, 3, KEY_BACK),
+	KEY(5, 2, KEY_SEND),
+	KEY(5, 3, KEY_HOME),
+	KEY(8, 0, KEY_END),
+	KEY(8, 1, KEY_VOLUMEUP),
+	KEY(8, 2, KEY_VOLUMEDOWN),
+};
+
+static struct matrix_keymap_data u5500_keymap_data = {
+	.keymap		= u5500_keymap,
+	.keymap_size	= ARRAY_SIZE(u5500_keymap),
+};
+
+static struct db5500_keypad_platform_data u5500_keypad_board = {
+	.keymap_data	= &u5500_keymap_data,
+	.no_autorepeat	= true,
+	.debounce_ms	= 40, /* milliseconds */
+};
 
 static void __init u5500_uart_init(void)
 {
@@ -33,6 +62,8 @@ static void __init u5500_init_machine(void)
 
 	u5500_sdi_init();
 	u5500_uart_init();
+
+	db5500_add_keypad(&u5500_keypad_board);
 }
 
 MACHINE_START(U5500, "ST-Ericsson U5500 Platform")
