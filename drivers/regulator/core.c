6e92f8528509673ac2efd1bfe98342f1c2466fb7
Philippe Langlais <philippe.langlais@linaro.org>
2011-03-24 14:12:01 +0100

    Regulators: show consumers that holds a regulator
    To locate the consumer(s) that currently holds
    (ie have enabled) a regulator, a new sysfs entry
    is created. The consumer(s) are published in
    /sys/class/regulator/regulator.<#>/use
    
    Signed-off-by: Martin Persson <martin.persson@stericsson.com>
    Change-Id: Ief78276c9685d0bf5688294b9aed9e0698c3475f
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/6549
    Reviewed-by: Bengt JONSSON <bengt.g.jonsson@stericsson.com>
    
    Conflicts:
    
    	drivers/regulator/core.c

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -85,6 +85,7 @@ struct regulator {
 #ifdef CONFIG_DEBUG_FS
 	struct dentry *debugfs;
 #endif
+	int use;
 };
 
 static int _regulator_is_enabled(struct regulator_dev *rdev);
@@ -579,6 +580,32 @@ static ssize_t regulator_suspend_standby_state_show(struct device *dev,
 static DEVICE_ATTR(suspend_standby_state, 0444,
 		regulator_suspend_standby_state_show, NULL);
 
+static ssize_t regulator_use_show(struct device *dev,
+				   struct device_attribute *attr, char *buf)
+{
+	struct regulator_dev *rdev = dev_get_drvdata(dev);
+	struct regulator *reg;
+	size_t size = 0;
+
+	if (rdev->use_count == 0)
+		return sprintf(buf, "no users\n");
+
+	list_for_each_entry(reg, &rdev->consumer_list, list) {
+		if (!reg->use)
+			continue;
+
+		if (reg->dev != NULL)
+			size += sprintf((buf + size), "%s (%d) ",
+					dev_name(reg->dev), reg->use);
+		else
+			size += sprintf((buf + size), "unknown (%d) ",
+					reg->use);
+	}
+	size += sprintf((buf + size), "\n");
+
+	return size;
+}
+static DEVICE_ATTR(use, 0444, regulator_use_show, NULL);
 
 /*
  * These are the only attributes are present for all regulators.
@@ -1428,6 +1455,8 @@ int regulator_enable(struct regulator *regulator)
 
 	if (ret != 0 && rdev->supply)
 		regulator_disable(rdev->supply);
+	else
+		regulator->use++;
 
 	return ret;
 }
@@ -1500,6 +1529,9 @@ int regulator_disable(struct regulator *regulator)
 
 	if (ret == 0 && rdev->supply)
 		regulator_disable(rdev->supply);
+
+	if (ret == 0)
+		regulator->use--;
 
 	return ret;
 }
@@ -2501,6 +2533,10 @@ static int add_regulator_attributes(struct regulator_dev *rdev)
 	struct device		*dev = &rdev->dev;
 	struct regulator_ops	*ops = rdev->desc->ops;
 	int			status = 0;
+
+	status = device_create_file(dev, &dev_attr_use);
+	if (status < 0)
+		dev_warn(dev, "Create sysfs file \"use\" failed");
 
 	/* some attributes need specific methods to be displayed */
 	if (ops->get_voltage || ops->get_voltage_sel) {
