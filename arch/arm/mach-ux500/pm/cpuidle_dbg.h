8fd80a9e61c1b6856b5d6f8d2b15e8e10e258b52
Philippe Langlais <philippe.langlais@linaro.org>
2011-04-04 16:51:20 +0200

    mach-ux500: pm: merge all 2.6.35 power management features
    Add cpu idle support,
    Re introduce temporary implementation of atomic regulators
    Signed-off-by: Philippe Langlais <philippe.langlais@linaro.org>

---
+++ arch/arm/mach-ux500/pm/cpuidle_dbg.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) ST-Ericsson SA 2010
+ *
+ * License Terms: GNU General Public License v2
+ * Author: Rickard Andersson <rickard.andersson@stericsson.com> for ST-Ericsson
+ *	   Jonas Aaberg <jonas.aberg@stericsson.com> for ST-Ericsson
+ */
+
+#ifndef CPUIDLE_DBG_H
+#define CPUIDLE_DBG_H
+
+#ifdef CONFIG_U8500_CPUIDLE_DEBUG
+void ux500_ci_dbg_init(void);
+void ux500_ci_dbg_remove(void);
+
+void ux500_ci_dbg_log(int state, int this_cpu);
+
+bool ux500_ci_dbg_force_ape_on(void);
+int ux500_ci_dbg_deepest_state(void);
+
+void ux500_ci_dbg_console(void);
+void ux500_ci_dbg_console_check_uart(void);
+void ux500_ci_dbg_console_handle_ape_resume(void);
+void ux500_ci_dbg_console_handle_ape_suspend(void);
+
+#ifdef U8500_CPUIDLE_EXTRA_DBG
+void ux500_ci_dbg_msg(char *dbg_string);
+#else
+static inline void ux500_ci_dbg_msg(char *dbg_string) { }
+#endif
+
+#else
+
+static inline void ux500_ci_dbg_init(void) { }
+static inline void ux500_ci_dbg_remove(void) { }
+
+static inline void ux500_ci_dbg_log(int state, int this_cpu) { }
+
+static inline bool ux500_ci_dbg_force_ape_on(void)
+{
+	return false;
+}
+
+static inline int ux500_ci_dbg_deepest_state(void)
+{
+	/* This means no lower sleep state than ApIdle */
+	return CONFIG_U8500_CPUIDLE_DEEPEST_STATE;
+}
+
+static inline void ux500_ci_dbg_console(void) { }
+static inline void ux500_ci_dbg_console_check_uart(void) { }
+static inline void ux500_ci_dbg_console_handle_ape_resume(void) { }
+static inline void ux500_ci_dbg_console_handle_ape_suspend(void) { }
+static inline void ux500_ci_dbg_msg(char *dbg_string) { }
+
+
+
+#endif
+#endif
