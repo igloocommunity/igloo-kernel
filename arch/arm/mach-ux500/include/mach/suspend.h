6d4bb96355cdf4a17cf019c4b481eb41bcef1803
Philippe Langlais <philippe.langlais@linaro.org>
2011-04-05 09:42:40 +0200

    mach-ux500: pm: add suspend.h
    Signed-off-by: Philippe Langlais <philippe.langlais@linaro.org>

---
+++ arch/arm/mach-ux500/include/mach/suspend.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) ST-Ericsson SA 2010
+ *
+ * License terms: GNU General Public License (GPL) version 2
+ */
+#ifndef __MACH_SUSPEND_H
+#define __MACH_SUSPEND_H
+
+void suspend_block_sleep(void);
+void suspend_unblock_sleep(void);
+
+#endif /* __MACH_SUSPEND_H */
