ab11f10bf81b0ce077fc498f96244db31739108b
Philippe Langlais <philippe.langlais@linaro.org>
2011-05-09 15:17:39 +0200

    board-mop500: Add cmdline param for GPIO config
    Allows ux500 derived boards to override the default GPIO
    configuration by passing in pinsfor= in the kernel
    command line.
    
    ST-Ericsson ID: 327707
    ST-Ericsson FOSS-OUT ID: Trivial
    
    Change-Id: I81ce4025a8cd9ed80fa3f4543ac35806e093f424
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/20364
    Reviewed-by: Pawel SZYSZUK <pawel.szyszuk@stericsson.com>
    Reviewed-by: Rabin VINCENT <rabin.vincent@stericsson.com>
    Reviewed-by: Srinidhi KASAGAR <srinidhi.kasagar@stericsson.com>
    Tested-by: Chris KIMBER <chris.kimber@stericsson.com>
    Reviewed-by: Jonas ABERG <jonas.aberg@stericsson.com>
    
    Conflicts:
    
    	arch/arm/mach-ux500/board-mop500-pins.c

--- arch/arm/mach-ux500/board-mop500-pins.c
+++ arch/arm/mach-ux500/board-mop500-pins.c
@@ -6,6 +6,7 @@
 
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/string.h>
 
 #include <asm/mach-types.h>
 #include <plat/pincfg.h>
@@ -13,6 +14,13 @@
 #include <mach/hardware.h>
 
 #include "pins-db8500.h"
+
+enum custom_pin_cfg_t {
+	PINS_FOR_DEFAULT,
+	PINS_FOR_U9500_21,
+};
+
+static enum custom_pin_cfg_t pinsfor = PINS_FOR_DEFAULT;
 
 static pin_cfg_t mop500_pins_common[] = {
 	/* I2C */
@@ -289,6 +297,11 @@ static pin_cfg_t hrefv60_pins[] = {
 
 	/* SD card detect */
 	GPIO95_GPIO	| PIN_INPUT_PULLUP,
+};
+
+static pin_cfg_t u9500_21_pins[] = {
+	GPIO4_U1_RXD    | PIN_INPUT_PULLUP,
+	GPIO5_U1_TXD    | PIN_OUTPUT_HIGH,
 };
 
 static pin_cfg_t snowball_pins[] = {
@@ -589,6 +602,21 @@ void mop500_pins_suspend_force_mux(void)
 
 }
 
+/*
+ * passing "pinsfor=" in kernel cmdline allows for custom
+ * configuration of GPIOs on u8500 derived boards.
+ */
+static int __init early_pinsfor(char *p)
+{
+	pinsfor = PINS_FOR_DEFAULT;
+
+	if (strcmp(p, "u9500-21") == 0)
+		pinsfor = PINS_FOR_U9500_21;
+
+	return 0;
+}
+early_param("pinsfor", early_pinsfor);
+
 void __init mop500_pins_init(void)
 {
 	nmk_config_pins(mop500_pins_common,
@@ -596,6 +624,16 @@ void __init mop500_pins_init(void)
 
 	nmk_config_pins(mop500_pins_default,
 			ARRAY_SIZE(mop500_pins_default));
+
+	switch (pinsfor) {
+	case PINS_FOR_U9500_21:
+		nmk_config_pins(u9500_21_pins, ARRAY_SIZE(u9500_21_pins));
+		break;
+
+	case PINS_FOR_DEFAULT:
+	default:
+		break;
+	}
 }
 
 void __init snowball_pins_init(void)
