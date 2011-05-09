be32867ea6467339cb778dcebc9120d2b3bddc8d
Rabin Vincent <rabin.vincent@stericsson.com>
2011-05-09 10:51:51 +0530

    u5500: add the Vape power state regulator
    ST-Ericsson Linux next: -
    ST-Ericsson ID: WP257121
    ST-Ericsson FOSS-OUT ID: Trivial
    
    Change-Id: I7a43e311c6bd7774911ab7efff1ccca7c1e4f23c
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/22692
    Tested-by: Rabin VINCENT <rabin.vincent@stericsson.com>
    Reviewed-by: Srinidhi KASAGAR <srinidhi.kasagar@stericsson.com>

---
+++ arch/arm/mach-ux500/regulator-u5500.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) ST-Ericsson SA 2011
+ *
+ * License Terms: GNU General Public License v2
+ */
+
+#ifndef __REGULATOR_U5500_H
+#define __REGULATOR_U5500_H
+
+enum u5500_regulator_id {
+	U5500_REGULATOR_VAPE,
+	U5500_NUM_REGULATORS
+};
+
+#endif
--- arch/arm/mach-ux500/regulator-ux500.c
+++ arch/arm/mach-ux500/regulator-ux500.c
@@ -364,8 +364,6 @@ ux500_regulator_probe(struct platform_device *pdev,
 			"regulator-%s-probed\n", info->desc.name);
 	}
 
-	regulator_has_full_constraints();
-
 	return 0;
 }
 
