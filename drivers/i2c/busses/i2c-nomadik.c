421a9c2d375ecfa69f6a2c4e0ad5e4e2c3e64087
Philippe Langlais <philippe.langlais@linaro.org>
2011-05-10 11:07:02 +0200

    i2c-nomadik: Reset the hw after status check
    In case of I2C timeout, reset the HW only after
    the HW status is read, otherwise the staus will
    be lost.
    
    ST-Ericsson ID: ER334949
    Change-Id: I93295122fa38d0ea82780d54af7f5de1fa099369
    Signed-off-by: Virupax Sadashivpetimath <virupax.sadashivpetimath@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/20986
    Reviewed-by: Jonas ABERG <jonas.aberg@stericsson.com>
    Reviewed-by: Srinidhi KASAGAR <srinidhi.kasagar@stericsson.com>
    
    Conflicts:
    
    	drivers/i2c/busses/i2c-nomadik.c

--- drivers/i2c/busses/i2c-nomadik.c
+++ drivers/i2c/busses/i2c-nomadik.c
@@ -429,10 +429,9 @@ static int read_i2c(struct nmk_i2c_dev *dev)
 	}
 
 	if (timeout == 0) {
-		/* controller has timedout, re-init the h/w */
+		/* Controller timed out */
 		dev_err(&dev->pdev->dev, "Read from Slave 0x%x timed out\n",
 				dev->cli.slave_adr);
-		(void) init_hw(dev);
 		status = -ETIMEDOUT;
 	}
 	return status;
@@ -517,10 +516,9 @@ static int write_i2c(struct nmk_i2c_dev *dev)
 	}
 
 	if (timeout == 0) {
-		/* controller has timedout, re-init the h/w */
+		/* Controller timed out */
 		dev_err(&dev->pdev->dev, "Write to slave 0x%x timed out\n",
 				dev->cli.slave_adr);
-		(void) init_hw(dev);
 		status = -ETIMEDOUT;
 	}
 
