55299d33f87d8a8cf299dd47854c1e279374b983
Philippe Langlais <philippe.langlais@linaro.org>
2011-05-11 15:59:12 +0200

    ux500: separate out cpufreq
    Move the 8500-specific tables to a separate file so the the common code can be
    reused for 5500.
    
    ST-Ericsson Linux next: -
    ST-Ericsson ID: WP257121
    ST-Ericsson FOSS-OUT ID: Trivial
    
    Change-Id: I1ca8daef15b9b92a770fed3341f6984f6b88048f
    Signed-off-by: Rabin Vincent <rabin.vincent@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/22829
    Reviewed-by: Jonas ABERG <jonas.aberg@stericsson.com>
    Reviewed-by: Martin PERSSON <martin.persson@stericsson.com>
    Reviewed-by: QATEST
    
    Conflicts:
    
    	arch/arm/mach-ux500/pm/Makefile
    	arch/arm/mach-ux500/pm/cpufreq.c

---
+++ arch/arm/mach-ux500/pm/cpufreq-db8500.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) ST-Ericsson SA 2011
+ *
+ * License Terms: GNU General Public License v2
+ */
+
+#include <linux/kernel.h>
+#include <linux/cpufreq.h>
+
+#include <mach/prcmu-fw-api.h>
+
+#include "cpufreq.h"
+
+static struct cpufreq_frequency_table freq_table[] = {
+	[0] = {
+		.index = 0,
+		.frequency = 300000,
+	},
+	[1] = {
+		.index = 1,
+		.frequency = 600000,
+	},
+	[2] = {
+		/* Used for MAX_OPP, if available */
+		.index = 2,
+		.frequency = CPUFREQ_TABLE_END,
+	},
+	[3] = {
+		.index = 3,
+		.frequency = CPUFREQ_TABLE_END,
+	},
+};
+
+static enum arm_opp idx2opp[] = {
+	ARM_50_OPP,
+	ARM_100_OPP,
+	ARM_MAX_OPP
+};
+
+static int __init u8500_cpufreq_register(void)
+{
+	BUILD_BUG_ON(ARRAY_SIZE(idx2opp) + 1 != ARRAY_SIZE(freq_table));
+
+	if (cpu_is_u8500v2() && !prcmu_is_u8400()) {
+		freq_table[0].frequency = 400000;
+		freq_table[1].frequency = 800000;
+		if (prcmu_has_arm_maxopp())
+			freq_table[2].frequency = 1000000;
+	}
+
+	return ux500_cpufreq_register(freq_table, idx2opp);
+}
+device_initcall(u8500_cpufreq_register);
