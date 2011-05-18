a7ef6a5c674be62204de4791e5fbb3a32da43b18
om prakash <omprakash.pal@stericsson.com>
2011-05-18 16:38:17 +0530

    kernel-doc: fix kernel-doc warnings
    fix the potential kernel-doc warnings
    
    ST-Ericsson Linux next: -
    ST-Ericsson ID: 327425
    ST-Ericsson FOSS-OUT ID: Trivial
    
    Signed-off-by: om prakash <omprakash.pal@stericsson.com>
    Change-Id: Ibaa302ddbaa8e223b81bd047fceaf7c3b97af491
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/23345
    Reviewed-by: Rabin VINCENT <rabin.vincent@stericsson.com>
    Reviewed-by: Jonas ABERG <jonas.aberg@stericsson.com>
    Reviewed-by: QATEST
    Reviewed-by: Srinidhi KASAGAR <srinidhi.kasagar@stericsson.com>

--- drivers/i2c/busses/i2c-nomadik.c
+++ drivers/i2c/busses/i2c-nomadik.c
@@ -135,18 +135,19 @@ struct i2c_nmk_client {
 };
 
 /**
- * struct nmk_i2c_dev - private data structure of the controller
- * @pdev: parent platform device
- * @adap: corresponding I2C adapter
- * @irq: interrupt line for the controller
- * @virtbase: virtual io memory area
- * @clk: hardware i2c block clock
- * @cfg: machine provided controller configuration
- * @cli: holder of client specific data
- * @stop: stop condition
- * @xfer_complete: acknowledge completion for a I2C message
- * @result: controller propogated result
- * @busy: Busy doing transfer
+ * struct nmk_i2c_dev - private data structure of the controller.
+ * @pdev: parent platform device.
+ * @adap: corresponding I2C adapter.
+ * @irq: interrupt line for the controller.
+ * @virtbase: virtual io memory area.
+ * @clk: hardware i2c block clock.
+ * @cfg: machine provided controller configuration.
+ * @cli: holder of client specific data.
+ * @stop: stop condition.
+ * @xfer_complete: acknowledge completion for a I2C message.
+ * @result: controller propogated result.
+ * @regulator: pointer to i2c regulator.
+ * @busy: Busy doing transfer.
  */
 struct nmk_i2c_dev {
 	struct platform_device		*pdev;
@@ -683,7 +684,7 @@ out:
 /**
  * disable_interrupts() - disable the interrupts
  * @dev: private data of controller
- * @irq: interrupt number
+ * @irq: interrupt number.
  */
 static int disable_interrupts(struct nmk_i2c_dev *dev, u32 irq)
 {
