471ac5f64b9b75938819733b0eea9bff13f33996
Bengt Jonsson <bengt.g.jonsson@stericsson.com>
2011-05-31 15:54:25 +0200

    ARM: ux500: regulators: add mask for configuration
    There is already a register mask in the regulator driver
    to allow some bits of a register to be initialized. The register
    value is defined in the board configuration. This patch puts a
    mask in the board configuration to specify which bits should
    actually be altered. The purpose with this patch is to avoid
    future mistakes when updating the allowed bits in the regulator
    driver.
    
    Change-Id: I55c531c9258854dbaf9c4ecb5786ca5d7bfdfcf2
    Signed-off-by: Bengt Jonsson <bengt.g.jonsson@stericsson.com>
    Signed-off-by: Mian Yousaf Kaukab <mian.yousaf.kaukab@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/32096
    Tested-by: Jonas ABERG <jonas.aberg@stericsson.com>

--- include/linux/regulator/ab8500.h
+++ include/linux/regulator/ab8500.h
@@ -29,13 +29,15 @@ enum ab8500_regulator_id {
 /* AB8500 register initialization */
 struct ab8500_regulator_reg_init {
 	int id;
+	u8 mask;
 	u8 value;
 };
 
-#define INIT_REGULATOR_REGISTER(_id, _value)	\
-	{					\
-		.id = _id,			\
-		.value = _value,		\
+#define INIT_REGULATOR_REGISTER(_id, _mask, _value)	\
+	{						\
+		.id = _id,				\
+		.mask = _mask,				\
+		.value = _value,			\
 	}
 
 /* AB8500 registers */
