fb1345575533a8bf30bf744833a3905d7465cb63
Robert Marklund <robert.marklund@stericsson.com>
2011-06-22 20:40:01 +0200

    mach-ux500: Add mmio device files and device
    Updates board_mop500.c  so that mmio driver can obtain gpio
    config from platform
    Adds sensor clock configuration to board specific file.
    Adds support for impplementing different power cycling schemes for
    primary and secondary camera sensors.
    
    Signed-off-by: Robert Marklund <robert.marklund@stericsson.com>

--- arch/arm/mach-ux500/include/mach/devices.h
+++ arch/arm/mach-ux500/include/mach/devices.h
@@ -30,6 +30,7 @@ extern struct platform_device u8500_thsens_device;
 extern struct platform_device u8500_dma40_device;
 extern struct platform_device ux500_ske_keypad_device;
 extern struct platform_device u8500_hsi_device;
+extern struct platform_device ux500_mmio_device;
 
 void dma40_u8500ed_fixup(void);
 
