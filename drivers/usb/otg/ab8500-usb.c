a0c2e019b2ed832107a775db90d51b70059eeec5
Sakethram Bommisetti <sakethram.bommisetti@stericsson.com>
2011-06-28 14:04:56 +0530

    USB:Software fix for usb eye diagram issue when drawing more than 100mA
    AB V2 has eye diagram issues when drawing more than 100mA
    from VBUS.So setting charging current to 100mA in case of standard host
    
    Change-Id: Iaa5eb5751a4b0665fbe6f6148b712ffa9f96a06b
    Signed-off-by: Sakethram Bommisetti <sakethram.bommisetti@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/25958
    Reviewed-by: Praveena NADAHALLY <praveen.nadahally@stericsson.com>
    Reviewed-by: Philippe LANGLAIS <philippe.langlais@stericsson.com>

--- drivers/usb/otg/ab8500-usb.c
+++ drivers/usb/otg/ab8500-usb.c
@@ -356,6 +356,21 @@ static void ab8500_usb_phy_disable_work(struct work_struct *work)
 		ab8500_usb_peri_phy_dis(ab);
 }
 
+static unsigned ab8500_eyediagram_workaroud(struct ab8500_usb *ab, unsigned mA)
+{
+	if (mA > 100) {
+		/* AB V2 has eye diagram issues when drawing more
+		 * than 100mA from VBUS.So setting charging current
+		 * to 100mA in case of standard host
+		 */
+		if (ab->rev < 0x30)
+			mA = 100;
+		else
+			mA = 300;
+	}
+	return mA;
+}
+
 static int ab8500_usb_set_power(struct otg_transceiver *otg, unsigned mA)
 {
 	struct ab8500_usb *ab;
@@ -364,6 +379,8 @@ static int ab8500_usb_set_power(struct otg_transceiver *otg, unsigned mA)
 		return -ENODEV;
 
 	ab = xceiv_to_ab(otg);
+
+	mA = ab8500_eyediagram_workaroud(ab, mA);
 
 	ab->vbus_draw = mA;
 
