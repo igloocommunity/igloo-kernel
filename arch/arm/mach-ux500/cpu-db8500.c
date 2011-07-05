ac6c0d6affb392367edf601f0e01e90f19fe8be9
Philippe Langlais <philippe.langlais@linaro.org>
2011-07-05 14:29:14 +0200

    mach-ux500: Fix board merge & cleanup
    Signed-off-by: Philippe Langlais <philippe.langlais@linaro.org>

--- arch/arm/mach-ux500/cpu-db8500.c
+++ arch/arm/mach-ux500/cpu-db8500.c
@@ -397,9 +397,6 @@ static void __init db8500_add_gpios(void)
 	if (cpu_is_u8500v2())
 		pdata.supports_sleepmode = true;
 
-	if (cpu_is_u8500v2())
-		pdata.supports_sleepmode = true;
-
 	dbx500_add_gpios(ARRAY_AND_SIZE(db8500_gpio_base),
 			 IRQ_DB8500_GPIO0, &pdata);
 }
@@ -499,14 +496,13 @@ static ssize_t ux500_get_process(char *buf, struct sysfs_soc_info *si)
 
 static ssize_t ux500_get_reset_code(char *buf, struct sysfs_soc_info *si)
 {
-	return 0;
-	//EROBEMA return sprintf(buf, "0x%04x\n", prcmu_get_reset_code());
+	return sprintf(buf, "0x%04x\n", prcmu_get_reset_code());
 }
 
 static ssize_t ux500_get_reset_reason(char *buf, struct sysfs_soc_info *si)
 {
-	return 0;
-	//EROBEMA return sprintf(buf, "%s\n", reboot_reason_string(prcmu_get_reset_code()));
+	return sprintf(buf, "%s\n",
+		reboot_reason_string(prcmu_get_reset_code()));
 }
 
 static struct sysfs_soc_info soc_info[] = {
@@ -515,7 +511,7 @@ static struct sysfs_soc_info soc_info[] = {
 	SYSFS_SOC_ATTR_CALLBACK("soc_id", ux500_get_soc_id),
 	SYSFS_SOC_ATTR_CALLBACK("revision", ux500_get_revision),
 	SYSFS_SOC_ATTR_CALLBACK("process", ux500_get_process),
-//EROBEMA	SYSFS_SOC_ATTR_CALLBACK("reset_code", ux500_get_reset_code),
+	SYSFS_SOC_ATTR_CALLBACK("reset_code", ux500_get_reset_code),
 	SYSFS_SOC_ATTR_CALLBACK("reset_reason", ux500_get_reset_reason),
 };
 
