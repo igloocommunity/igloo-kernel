b1cb3a0217f8e8d7b035dbe36b41e68665e489b4
Naveen Kumar Gaddipati <naveen.gaddipati@stericsson.com>
2011-09-06 20:48:48 +0530

    mach-ux500:Platform data for kernel 3.0
    Platform data for kernel 3.0 migration.
    
    ST-Ericsson ID: 352334
    ST-Ericsson Linux next: NA
    ST-Ericsson FOSS-OUT ID: NA
    
    Signed-off-by: Naveen Kumar Gaddipati <naveen.gaddipati@stericsson.com>

--- arch/arm/mach-ux500/board-u5500.c
+++ arch/arm/mach-ux500/board-u5500.c
@@ -520,7 +520,7 @@ static void __init u5500_uart_init(void)
 	db5500_add_uart0(NULL);
 	db5500_add_uart1(NULL);
 	db5500_add_uart2(NULL);
-	db5500_add_uart3();
+	db5500_add_uart3(NULL);
 }
 
 static void __init u5500_init_machine(void)
--- arch/arm/mach-ux500/hotplug.c
+++ arch/arm/mach-ux500/hotplug.c
@@ -11,21 +11,22 @@
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/smp.h>
+#include <linux/completion.h>
 
 #include <asm/cacheflush.h>
 
-#ifdef CONFIG_U8500_CPUIDLE
-#include "pm/cpuidle.h"
-#endif
 #include "pm/context.h"
 
 extern volatile int pen_release;
+
+static DECLARE_COMPLETION(cpu_killed);
 
 static inline void platform_do_lowpower(unsigned int cpu)
 {
 	flush_cache_all();
 
 	for (;;) {
+
 		context_varm_save_core();
 		context_save_cpu_registers();
 
@@ -45,7 +46,7 @@ static inline void platform_do_lowpower(unsigned int cpu)
 
 int platform_cpu_kill(unsigned int cpu)
 {
-	return 1;
+	return wait_for_completion_timeout(&cpu_killed, 5000);
 }
 
 /*
@@ -55,6 +56,19 @@ int platform_cpu_kill(unsigned int cpu)
  */
 void platform_cpu_die(unsigned int cpu)
 {
+#ifdef DEBUG
+	unsigned int this_cpu = hard_smp_processor_id();
+
+	if (cpu != this_cpu) {
+		printk(KERN_CRIT "Eek! platform_cpu_die running on %u, should be %u\n",
+			   this_cpu, cpu);
+		BUG();
+	}
+#endif
+
+	printk(KERN_NOTICE "CPU%u: shutdown\n", cpu);
+	complete(&cpu_killed);
+
 	/* directly enter low power state, skipping secure registers */
 	platform_do_lowpower(cpu);
 }
