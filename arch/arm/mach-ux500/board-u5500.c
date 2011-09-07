aa8e4eb9da7c51aca1cc7e85e5db7ff0a500a7f5
Virupax Sadashivpetimath <virupax.sadashivpetimath@stericsson.com>
2011-09-07 18:05:58 +0530

    Revert "cg2900: bluetooth support for 5500"
    This reverts commit 76d004cce0dbb6fddad256790e6ae574278b46e0.
    
    Conflicts:
    
    	arch/arm/mach-ux500/board-u5500.c
    	drivers/staging/cg2900/devices-cg2900-u8500.c
    	drivers/staging/cg2900/devices-cg2900.h
    
    Change-Id: Ic480f348be5387d83a3e46280b73dd9bbb3ba43d
    Signed-off-by: Virupax Sadashivpetimath <virupax.sadashivpetimath@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/30347
    Reviewed-by: Par-Gunnar HJALMDAHL <par-gunnar.p.hjalmdahl@stericsson.com>

--- arch/arm/mach-ux500/board-u5500.c
+++ arch/arm/mach-ux500/board-u5500.c
@@ -520,7 +520,6 @@ static void __init u5500_uart_init(void)
 	db5500_add_uart0(NULL);
 	db5500_add_uart1(NULL);
 	db5500_add_uart2(NULL);
-	db5500_add_uart3(NULL);
 }
 
 static void __init u5500_init_machine(void)
