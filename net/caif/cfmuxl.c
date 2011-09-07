7f6175fd33162ac9ec4ef81cecdb47580f629473
Hemant Ramdasi <hemant.ramdasi@stericsson.com>
2011-09-07 16:22:15 +0530

    Revert "caif fix merge problems merge with org commit."
    This reverts commit 4e5176a567166769c29a39f66959b81efae11e47.
    
    Signed-off-by: Hemant Ramdasi <hemant.ramdasi@stericsson.com>
    Change-Id: I66de7f9a713c056ce974355e6c28feac2282aec7
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/30327

--- net/caif/cfmuxl.c
+++ net/caif/cfmuxl.c
@@ -254,5 +254,6 @@ static void cfmuxl_ctrlcmd(struct cflayer *layr, enum caif_ctrlcmd ctrl,
 		if (cfsrvl_phyid_match(layer, phyid))
 			layer->ctrlcmd(layer, ctrl, phyid);
 		}
+	}
 	rcu_read_unlock();
 }
