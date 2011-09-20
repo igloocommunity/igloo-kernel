6ff6187320a1ba822a2dafdb1afa7f54c5533e2f
Mian Yousaf Kaukab <mian.yousaf.kaukab@stericsson.com>
2011-09-20 09:05:11 +0200

    gpio: ab8500: restore gpio driver to mainline state
    Driver is restored to linux-linaro-3.0-2011.07-1 tag.
    
    Change-Id: I25a840663b6d900efc7355c9f87e907e637dd927
    Signed-off-by: Mian Yousaf Kaukab <mian.yousaf.kaukab@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/32092
    Reviewed-by: Jonas ABERG <jonas.aberg@stericsson.com>
    Tested-by: Jonas ABERG <jonas.aberg@stericsson.com>

--- include/linux/mfd/ab8500/gpio.h
+++ include/linux/mfd/ab8500/gpio.h
@@ -15,19 +15,7 @@
 struct ab8500_gpio_platform_data {
 	int gpio_base;
 	u32 irq_base;
-	u8  initial_pin_config[7];
-	u8  initial_pin_direction[6];
-	u8  initial_pin_pullups[6];
+	u8  config_reg[7];
 };
 
-int ab8500_config_pull_up_or_down(struct device *dev,
-				unsigned ab8500_gpio_offset, bool enable);
-
-int ab8500_gpio_config_select(struct device *dev,
-			unsigned ab8500_gpio_offset, bool gpio_select);
-
-int ab8500_gpio_config_get_select(struct device *dev,
-			unsigned ab8500_gpio_offset, bool *gpio_select);
-
 #endif /* _AB8500_GPIO_H */
-
