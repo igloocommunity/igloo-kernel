1f7d7a45e414156bc6a6c7a576d8c2c0609ed060
Jonas Aaberg <jonas.aberg@stericsson.com>
2011-09-20 13:33:32 +0200

    ARM: ux500: suspend: Force gpio pins
    Change-Id: I2625a7804544731961454f264a36baf29cd60221
    Signed-off-by: Jonas Aaberg <jonas.aberg@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/32082

--- arch/arm/mach-ux500/include/mach/suspend.h
+++ arch/arm/mach-ux500/include/mach/suspend.h
@@ -9,9 +9,12 @@
 #ifdef CONFIG_UX500_SUSPEND
 void suspend_block_sleep(void);
 void suspend_unblock_sleep(void);
+void suspend_set_pins_force_fn(void (*force)(void), void (*force_mux)(void));
 #else
 static inline void suspend_block_sleep(void) { }
 static inline void suspend_unblock_sleep(void) { }
+static inline void suspend_set_pins_force_fn(void (*force)(void),
+					     void (*force_mux)(void)) { }
 #endif
 
 #endif /* __MACH_SUSPEND_H */
