078e13d22090520f7590f59d9b2f519cc41998bc
Jonas Aaberg <jonas.aberg@stericsson.com>
2011-09-20 08:04:18 +0200

    ARM: ux500: Add suspend support
    Change-Id: I039557aed52693dfad9b76ea0f2f746193aa4316
    Signed-off-by: Jonas Aaberg <jonas.aberg@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/32074

--- arch/arm/mach-ux500/hotplug.c
+++ arch/arm/mach-ux500/hotplug.c
@@ -15,7 +15,11 @@
 
 #include <asm/cacheflush.h>
 
+#include <mach/context.h>
+
 extern volatile int pen_release;
+
+static DECLARE_COMPLETION(cpu_killed);
 
 static inline void platform_do_lowpower(unsigned int cpu)
 {
@@ -23,12 +27,17 @@ static inline void platform_do_lowpower(unsigned int cpu)
 
 	for (;;) {
 
-		__asm__ __volatile__("dsb\n\t" "wfi\n\t"
-				     : : : "memory");
+		context_varm_save_core();
+		context_save_cpu_registers();
+
+		context_save_to_sram_and_wfi(false);
+
+		context_restore_cpu_registers();
+		context_varm_restore_core();
 
 		if (pen_release == cpu) {
 			/*
-			 * OK, proper wakeup, we're done
+			* OK, proper wakeup, we're done
 			 */
 			break;
 		}
@@ -37,7 +46,7 @@ static inline void platform_do_lowpower(unsigned int cpu)
 
 int platform_cpu_kill(unsigned int cpu)
 {
-	return 1;
+	return wait_for_completion_timeout(&cpu_killed, 5000);
 }
 
 /*
@@ -47,6 +56,19 @@ int platform_cpu_kill(unsigned int cpu)
  */
 void platform_cpu_die(unsigned int cpu)
 {
+#ifdef DEBUG
+	unsigned int this_cpu = hard_smp_processor_id();
+
+	if (cpu != this_cpu) {
+		printk(KERN_CRIT "Eek! platform_cpu_die running on %u, should be %u\n",
+			   this_cpu, cpu);
+		BUG();
+	}
+#endif
+
+	printk(KERN_NOTICE "CPU%u: shutdown\n", cpu);
+	complete(&cpu_killed);
+
 	/* directly enter low power state, skipping secure registers */
 	platform_do_lowpower(cpu);
 }
