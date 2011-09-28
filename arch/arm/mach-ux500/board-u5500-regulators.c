1d3cd3bbf79f95b8f829f1d3a4f1708e1b183b54
Venkata Biswanath <venkata.biswanath@stericsson.com>
2011-09-28 12:22:31 +0530

    u5500: prcmu: move and update power state/epod consumers
    Change-Id: Ieb54928cd12ed2fe8129b58d546f165ac58d9d00
    Signed-off-by: Venkata Biswanath <venkata.biswanath@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/32447
    Reviewed-by: Jonas ABERG <jonas.aberg@stericsson.com>
    Tested-by: Jonas ABERG <jonas.aberg@stericsson.com>

--- arch/arm/mach-ux500/board-u5500-regulators.c
+++ arch/arm/mach-ux500/board-u5500-regulators.c
@@ -142,103 +142,6 @@ struct ab5500_regulator_platform_data u5500_ab5500_regulator_data = {
 	.num_regulator	= ARRAY_SIZE(ab5500_regulator_init_data),
 };
 
-/*
- * Power state, ePOD, etc.
- */
-
-static struct regulator_consumer_supply u5500_vape_consumers[] = {
-	REGULATOR_SUPPLY("v-ape", NULL),
-	REGULATOR_SUPPLY("v-i2c", "nmk-i2c.0"),
-	REGULATOR_SUPPLY("v-i2c", "nmk-i2c.1"),
-	REGULATOR_SUPPLY("v-i2c", "nmk-i2c.2"),
-	REGULATOR_SUPPLY("v-i2c", "nmk-i2c.3"),
-	REGULATOR_SUPPLY("vcore", "sdi0"),
-	REGULATOR_SUPPLY("vcore", "sdi1"),
-	REGULATOR_SUPPLY("vcore", "sdi2"),
-	REGULATOR_SUPPLY("vcore", "sdi3"),
-	REGULATOR_SUPPLY("vcore", "sdi4"),
-	REGULATOR_SUPPLY("v-uart", "uart0"),
-	REGULATOR_SUPPLY("v-uart", "uart1"),
-	REGULATOR_SUPPLY("v-uart", "uart2"),
-	REGULATOR_SUPPLY("v-uart", "uart3"),
-	REGULATOR_SUPPLY("v-ape", "db5500-keypad"),
-};
-
-static struct regulator_consumer_supply u5500_sga_consumers[] = {
-	REGULATOR_SUPPLY("debug", "reg-virt-consumer.0"),
-	REGULATOR_SUPPLY("v-mali", NULL),
-};
-
-static struct regulator_consumer_supply u5500_hva_consumers[] = {
-	REGULATOR_SUPPLY("debug", "reg-virt-consumer.1"),
-	REGULATOR_SUPPLY("v-hva", NULL),
-};
-
-static struct regulator_consumer_supply u5500_sia_consumers[] = {
-	REGULATOR_SUPPLY("debug", "reg-virt-consumer.2"),
-	REGULATOR_SUPPLY("v-sia", "mmio_camera"),
-};
-
-static struct regulator_consumer_supply u5500_disp_consumers[] = {
-	REGULATOR_SUPPLY("debug", "reg-virt-consumer.3"),
-	REGULATOR_SUPPLY("vsupply", "b2r2_bus"),
-	REGULATOR_SUPPLY("vsupply", "mcde"),
-};
-
-static struct regulator_consumer_supply u5500_esram12_consumers[] = {
-	REGULATOR_SUPPLY("debug", "reg-virt-consumer.4"),
-	REGULATOR_SUPPLY("v-esram12", "mcde"),
-	REGULATOR_SUPPLY("esram12", "hva"),
-};
-
-#define U5500_REGULATOR_SWITCH(lower, upper)                            \
-[U5500_REGULATOR_SWITCH_##upper] = (struct regulator_init_data []) {    \
-{                                                                       \
-	.constraints = {                                                \
-		.valid_ops_mask = REGULATOR_CHANGE_STATUS,              \
-	},                                                              \
-	.consumer_supplies      = u5500_##lower##_consumers,            \
-	.num_consumer_supplies  = ARRAY_SIZE(u5500_##lower##_consumers),\
-}                                                                       \
-}
-
-/*
- * EPODs that require VAPE as supply regulator
- */
-
-#define U5500_REGULATOR_SWITCH_WITH_SUPPLY(lower, upper)                \
-[U5500_REGULATOR_SWITCH_##upper] = (struct regulator_init_data []) {    \
-{                                                                       \
-	.supply_regulator = "u5500-vape",                               \
-	.constraints = {                                                \
-		.valid_ops_mask = REGULATOR_CHANGE_STATUS,              \
-	},                                                              \
-	.consumer_supplies      = u5500_##lower##_consumers,            \
-	.num_consumer_supplies  = ARRAY_SIZE(u5500_##lower##_consumers),\
-}                                                                       \
-}
-
-static struct regulator_init_data *
-u5500_regulator_init_data[U5500_NUM_REGULATORS] __initdata = {
-	[U5500_REGULATOR_VAPE] = (struct regulator_init_data []) {
-		{
-			.constraints = {
-				.valid_ops_mask	= REGULATOR_CHANGE_STATUS,
-			},
-			.consumer_supplies	= u5500_vape_consumers,
-			.num_consumer_supplies	= ARRAY_SIZE(u5500_vape_consumers),
-		}
-	},
-	U5500_REGULATOR_SWITCH_WITH_SUPPLY(sga, SGA),
-	U5500_REGULATOR_SWITCH_WITH_SUPPLY(hva, HVA),
-	U5500_REGULATOR_SWITCH_WITH_SUPPLY(sia, SIA),
-	U5500_REGULATOR_SWITCH_WITH_SUPPLY(disp, DISP),
-	/*
-	 * ESRAM12 is put in rentention by FW on VAPE off
-	 * no need to hold VAPE
-	 */
-	U5500_REGULATOR_SWITCH(esram12, ESRAM12),
-};
 
 static void __init u5500_regulators_init_debug(void)
 {
@@ -270,10 +173,6 @@ static struct fixed_voltage_config u5500_vio_pdata __initdata = {
 void __init u5500_regulators_init(void)
 {
 	u5500_regulators_init_debug();
-
-	platform_device_register_data(NULL, "u5500-regulators", -1,
-			u5500_regulator_init_data,
-			sizeof(u5500_regulator_init_data));
 
 	platform_device_register_data(NULL, "reg-fixed-voltage", -1,
 				      &u5500_vio_pdata,
