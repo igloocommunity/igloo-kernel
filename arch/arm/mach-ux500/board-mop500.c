dbe7fc5022feccd172878b8bf6af9b20975ba17e
Jonas Aaberg <jonas.aberg@stericsson.com>
2011-09-30 11:59:44 +0200

    ARM: ux500: Remove outdated sensors1p driver
    Replaced by gpio-keyboard implementation.
    
    ST-Ericsson Linux next: -
    ST-Ericsson ID: -
    ST-Ericsson FOSS-OUT ID: Trivial
    
    Change-Id: Ic6c8ea1d83c4b3d8ad6bbd9aa06ff30213cab6be
    Signed-off-by: Jonas Aaberg <jonas.aberg@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/32695

--- arch/arm/mach-ux500/board-mop500.c
+++ arch/arm/mach-ux500/board-mop500.c
@@ -48,7 +48,6 @@
 #include <mach/hardware.h>
 #include <mach/setup.h>
 #include <mach/devices.h>
-#include <mach/sensors1p.h>
 #include <mach/abx500-accdet.h>
 #include <mach/irqs.h>
 #include <mach/ste-dma40-db8500.h>
@@ -851,30 +850,6 @@ struct platform_device u8500_sim_detect_device = {
 };
 #endif
 
-#ifdef CONFIG_SENSORS1P_MOP
-static struct sensors1p_config sensors1p_config = {
-      /* SFH7741 */
-       .proximity = {
-               .pin = EGPIO_PIN_7,
-               .startup_time = 120, /* ms */
-               .regulator = "v-proximity",
-       },
-       /* HED54XXU11 */
-       .hal = {
-               .pin = EGPIO_PIN_8,
-               .startup_time = 100, /* Actually, I have no clue. */
-               .regulator = "v-hal",
-       },
-};
-
-struct platform_device sensors1p_device = {
-       .name = "sensors1p",
-       .dev = {
-               .platform_data = (void *)&sensors1p_config,
-       },
-};
-#endif
-
 static struct cryp_platform_data u8500_cryp1_platform_data = {
 	.mem_to_engine = {
 		.dir = STEDMA40_MEM_TO_PERIPH,
@@ -913,9 +888,6 @@ static struct hash_platform_data u8500_hash1_platform_data = {
 
 /* add any platform devices here - TODO */
 static struct platform_device *mop500_platform_devs[] __initdata = {
-#ifdef CONFIG_SENSORS1P_MOP
-       &sensors1p_device,
-#endif
 #ifdef CONFIG_U8500_SIM_DETECT
 	&u8500_sim_detect_device,
 #endif
