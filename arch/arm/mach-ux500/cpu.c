3c1070063d48c0806a40102eec2e00bab855d548
Philippe Langlais <philippe.langlais@stericsson.com>
2011-10-14 09:36:23 +0200

    mach-ux500: Expose PRCMU TCDM reset reason in /sys
    At reboot the reset reason string is converted into a 16bit number which is
    stored in the PRCMUs TCDM (a memory which survives a soft reset). This
    commit exposes this value upon the next startup to userspace through a
    sysfs entry.
    
    ST-Ericsson ID: 280431
    ST-Ericsson FOSS-OUT ID: Trivial
    
    Change-Id: Ic905f2e1823e203dfa78e616ecfe0193439ed805
    Signed-off-by: Sebastian Rasmussen <sebastian.rasmussen@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/22851
    Reviewed-by: Mattias NILSSON <mattias.i.nilsson@stericsson.com>
    Reviewed-by: QATEST
    Reviewed-by: Jonas ABERG <jonas.aberg@stericsson.com>

--- arch/arm/mach-ux500/cpu.c
+++ arch/arm/mach-ux500/cpu.c
@@ -26,36 +26,11 @@
 
 void __iomem *_PRCMU_BASE;
 
-/*
- * The reboot reason string can be 255 characters long and the memory
- * in which we save the sw reset reason is 2 bytes. Therefore we need to
- * convert the string into a 16 bit pattern.
- *
- * See file reboot_reasons.h for conversion.
- */
-static unsigned short map_cmd_to_code(const char *cmd)
-{
-	int i;
-
-	if (cmd == NULL)
-		/* normal reboot w/o argument */
-		return SW_RESET_NO_ARGUMENT;
-
-	/* Search through reboot reason list */
-	for (i = 0; i < reboot_reasons_size; i++) {
-		if (!strcmp(reboot_reasons[i].reason, cmd))
-			return reboot_reasons[i].code;
-	}
-
-	/* No valid Reboot Reason found */
-	return SW_RESET_CRASH;
-}
-
 void ux500_restart(char mode, const char *cmd)
 {
 	unsigned short reset_code;
 
-	reset_code = map_cmd_to_code(cmd);
+	reset_code = reboot_reason_code(cmd);
 	prcmu_system_reset(reset_code);
 
 	mdelay(1000);
