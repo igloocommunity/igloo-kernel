75e3cf2b8c77fb536b6c8de2d3cbbfe21c7afc7a
Philippe Langlais <philippe.langlais@stericsson.com>
2011-10-19 11:28:31 +0200

    mach-ux500: all the necessary for abx500 platform data
    Signed-off-by: Philippe Langlais <philippe.langlais@stericsson.com>

--- arch/arm/mach-ux500/board-mop500.c
+++ arch/arm/mach-ux500/board-mop500.c
@@ -29,6 +29,7 @@
 #include <linux/smsc911x.h>
 #include <linux/gpio_keys.h>
 #include <linux/delay.h>
+#include <linux/mfd/ab8500/denc.h>
 #include <linux/leds_pwm.h>
 #include <linux/pwm_backlight.h>
 
@@ -44,12 +45,26 @@
 #include <mach/setup.h>
 #include <mach/devices.h>
 #include <mach/irqs.h>
+#include <mach/ste_audio.h>
 
 #include "pins-db8500.h"
 #include "ste-dma40-db8500.h"
 #include "devices-db8500.h"
 #include "board-mop500.h"
 #include "board-mop500-regulators.h"
+#include "board-mop500-bm.h"
+
+#ifdef CONFIG_AB8500_DENC
+static struct ab8500_denc_platform_data ab8500_denc_pdata = {
+	.ddr_enable = true,
+	.ddr_little_endian = false,
+};
+#endif
+
+static struct ab8500_audio_platform_data ab8500_audio_plat_data = {
+	.ste_gpio_altf_init = msp13_i2s_init,
+	.ste_gpio_altf_exit = msp13_i2s_exit,
+};
 
 static struct gpio_led snowball_led_array[] = {
 	{
@@ -190,6 +205,15 @@ static struct ab8500_platform_data ab8500_platdata = {
 	.num_regulator_reg_init	= ARRAY_SIZE(ab8500_regulator_reg_init),
 	.regulator	= ab8500_regulators,
 	.num_regulator	= ARRAY_SIZE(ab8500_regulators),
+#ifdef CONFIG_AB8500_DENC
+	.denc		= &ab8500_denc_pdata,
+#endif
+	.audio		= &ab8500_audio_plat_data,
+	.battery	= &ab8500_bm_data,
+	.charger	= &ab8500_charger_plat_data,
+	.btemp		= &ab8500_btemp_plat_data,
+	.fg		= &ab8500_fg_plat_data,
+	.chargalg	= &ab8500_chargalg_plat_data,
 	.gpio		= &ab8500_gpio_pdata,
 };
 
