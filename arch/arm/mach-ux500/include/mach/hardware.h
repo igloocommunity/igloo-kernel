7107187aa1e4257f9163b5cbe4c36a404c20381d
Philippe Langlais <philippe.langlais@stericsson.com>
2011-10-20 09:41:48 +0200

    ARM: ux500: Remove u8500 ED and V1 support
    ST-Ericsson Linux next: Not tested, ask SSM for ER
    ST-Ericsson ID: 342987
    ST-Ericsson FOSS-OUT ID: Trivial
    
    Change-Id: Ib41941b01f350e505d7a95f3834dd3a12e5d1f28
    Signed-off-by: Jonas Aaberg <jonas.aberg@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/24289
    Reviewed-by: QATEST
    Reviewed-by: Linus WALLEIJ <linus.walleij@stericsson.com>

--- arch/arm/mach-ux500/include/mach/hardware.h
+++ arch/arm/mach-ux500/include/mach/hardware.h
@@ -10,20 +10,21 @@
 #ifndef __MACH_HARDWARE_H
 #define __MACH_HARDWARE_H
 
-/* macros to get at IO space when running virtually
+/*
+ * Macros to get at IO space when running virtually
  * We dont map all the peripherals, let ioremap do
  * this for us. We map only very basic peripherals here.
  */
 #define U8500_IO_VIRTUAL	0xf0000000
 #define U8500_IO_PHYSICAL	0xa0000000
 
-/* this macro is used in assembly, so no cast */
+/* This macro is used in assembly, so no cast */
 #define IO_ADDRESS(x)           \
 	(((x) & 0x0fffffff) + (((x) >> 4) & 0x0f000000) + U8500_IO_VIRTUAL)
 
 /* typesafe io address */
 #define __io_address(n)		__io(IO_ADDRESS(n))
-/* used by some plat-nomadik code */
+/* Used by some plat-nomadik code */
 #define io_p2v(n)		__io_address(n)
 
 #include <mach/db8500-regs.h>
@@ -32,12 +33,12 @@
 /*
  * FIFO offsets for IPs
  */
-#define MSP_TX_RX_REG_OFFSET	(0)
-#define SSP_TX_RX_REG_OFFSET	(0x8)
-#define SPI_TX_RX_REG_OFFSET	(0x8)
-#define SD_MMC_TX_RX_REG_OFFSET (0x80)
-#define CRYP1_RX_REG_OFFSET	(0x10)
-#define CRYP1_TX_REG_OFFSET	(0x8)
+#define MSP_TX_RX_REG_OFFSET	0
+#define SSP_TX_RX_REG_OFFSET	0x8
+#define SPI_TX_RX_REG_OFFSET	0x8
+#define SD_MMC_TX_RX_REG_OFFSET 0x80
+#define CRYP1_RX_REG_OFFSET	0x10
+#define CRYP1_TX_REG_OFFSET	0x8
 
 /* MSP related board specific declaration************************/
 
