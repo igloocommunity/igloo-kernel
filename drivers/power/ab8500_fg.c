fa1b03556a874204d3f8232b8627f92da0f3548a
Kalle Komierowski <karl.komierowski@stericsson.com>
2011-10-21 13:32:51 +0200

    power: ab8500_bm: Change node name to a generic name
    A simple rename of the /sys/ab8500_fg/charge_full to
    /sys/battery/charge_full to make it usable on more platforms.
    
    ST-Ericsson ID: 358829
    ST-Ericsson Linux next: NA
    ST-Ericsson FOSS-OUT ID: Trivial
    
    Change-Id: I9e2a711fb62876b44a1957ec508b087aaa1ca42c
    Signed-off-by: Kalle Komierowski <karl.komierowski@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/34910
    Reviewed-by: Peter ENDERBORG <peter.enderborg@stericsson.com>
    Reviewed-by: Jonas ABERG <jonas.aberg@stericsson.com>

--- drivers/power/ab8500_fg.c
+++ drivers/power/ab8500_fg.c
@@ -2034,7 +2034,7 @@ static int ab8500_fg_sysfs_init(struct ab8500_fg *di)
 
 	ret = kobject_init_and_add(&di->fg_kobject,
 		&ab8500_fg_ktype,
-		NULL, "ab8500_fg");
+		NULL, "battery");
 	if (ret < 0)
 		dev_err(di->dev, "failed to create sysfs entry\n");
 
