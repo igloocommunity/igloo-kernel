fd7aeaf47b909e81824ea8be2a610f6c2089a7e7
Philippe Langlais <philippe.langlais@stericsson.com>
2011-11-15 16:00:12 +0100

    Revert "usb: musb: drop a gigantic amount of ifdeferry"
    This reverts commit 622859634a663c5e55d0e2a2cdbb55ac058d97b3.
    
    Conflicts:
    
    	drivers/usb/musb/musb_core.c

--- drivers/usb/musb/musb_gadget.c
+++ drivers/usb/musb/musb_gadget.c
@@ -2029,7 +2029,9 @@ static int musb_gadget_stop(struct usb_gadget *g,
 
 	spin_lock_irqsave(&musb->lock, flags);
 
+#ifdef	CONFIG_USB_MUSB_OTG
 	musb_hnp_stop(musb);
+#endif
 
 	(void) musb_gadget_vbus_draw(&musb->g, 0);
 
@@ -2143,6 +2145,7 @@ void musb_g_disconnect(struct musb *musb)
 
 	switch (musb->xceiv->state) {
 	default:
+#ifdef	CONFIG_USB_MUSB_OTG
 		dev_dbg(musb->controller, "Unhandled disconnect %s, setting a_idle\n",
 			otg_state_string(musb->xceiv->state));
 		musb->xceiv->state = OTG_STATE_A_IDLE;
@@ -2154,6 +2157,7 @@ void musb_g_disconnect(struct musb *musb)
 		break;
 	case OTG_STATE_B_WAIT_ACON:
 	case OTG_STATE_B_HOST:
+#endif
 	case OTG_STATE_B_PERIPHERAL:
 	case OTG_STATE_B_IDLE:
 		musb->xceiv->state = OTG_STATE_B_IDLE;
