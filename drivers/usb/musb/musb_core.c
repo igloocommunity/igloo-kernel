1a457e53c470eef2410813c500c51ef15a861b45
maheswarudu <v.maheswarudu@stericsson.com>
2011-12-01 11:16:45 +0100

    ux500: USB: platform specific musb fifo cfg
    Pass the musb fifo configuration through ux500
    platform file.
    
    ST-Ericsson ID: 367200
    ST-Ericsson FOSS-OUT ID: NA
    ST-Ericsson Linux next: NA
    
    Signed-off-by: maheswarudu <v.maheswarudu@stericsson.com>
    Change-Id: If7aaddeb85198dc9edce605efec9a87bbfb6658c
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/40604
    Reviewed-by: QATOOLS
    Reviewed-by: QABUILD
    Tested-by: Tobias ANDERSON <tobias.xx.andersson@stericsson.com>
    Reviewed-by: Rajaram REGUPATHY <ragupathy.rajaram@stericsson.com>
    Reviewed-by: Rabin VINCENT <rabin.vincent@stericsson.com>
    
    Conflicts:
    
    	drivers/usb/musb/musb_core.c

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -1018,9 +1018,6 @@ static void musb_shutdown(struct platform_device *pdev)
 	|| defined(CONFIG_USB_MUSB_AM35X)		\
 	|| defined(CONFIG_USB_MUSB_AM35X_MODULE)
 static ushort __initdata fifo_mode = 4;
-#elif defined(CONFIG_USB_MUSB_UX500)			\
-	|| defined(CONFIG_USB_MUSB_UX500_MODULE)
-static ushort __initdata fifo_mode = 5;
 #else
 static ushort __initdata fifo_mode = 2;
 #endif
