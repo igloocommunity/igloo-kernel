e2fe823759e721307c63bd25b55e6955de8276a8
Philippe Langlais <philippe.langlais@stericsson.com>
2011-12-01 14:25:28 +0100

    mach-ux500: gpio/nomadik: move the Nomadik GPIO header
    This takes the Nomadik GPIO header out of the plat-nomadik
    directory and pushes it down into <gpio/nomadik> where it belongs.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/cpu-db8500.c
+++ arch/arm/mach-ux500/cpu-db8500.c
@@ -14,6 +14,7 @@
 #include <linux/amba/bus.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
+#include <linux/gpio/nomadik.h>
 #include <linux/platform_device.h>
 #include <linux/io.h>
 #include <linux/dma-mapping.h>
@@ -21,7 +22,6 @@
 
 #include <asm/mach/map.h>
 #include <asm/pmu.h>
-#include <plat/gpio-nomadik.h>
 #include <mach/hardware.h>
 #include <mach/setup.h>
 #include <mach/devices.h>
