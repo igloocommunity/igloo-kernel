a79b1ebce818629037f54c5dcf143cb1195cae5d
Mathieu J. Poirier <mathieu.poirier@linaro.org>
2011-12-02 14:02:47 -0700

    snowball: android: Fixing interface issue after merge
    Function 'rndis_bind_config' ended up with the wrong amount
    of parameters after androidizing.
    
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

--- drivers/usb/gadget/u_ether.h
+++ drivers/usb/gadget/u_ether.h
@@ -112,8 +112,7 @@ int eem_bind_config(struct usb_configuration *c);
 
 #ifdef USB_ETH_RNDIS
 
-int rndis_bind_config(struct usb_configuration *c, u8 ethaddr[ETH_ALEN],
-				u32 vendorID, const char *manufacturer);
+int rndis_bind_config(struct usb_configuration *c, u8 ethaddr[ETH_ALEN]);
 
 #else
 
