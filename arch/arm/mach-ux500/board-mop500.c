66bcca3211ecbad4ffca056e84a578fa070d6aa7
Philippe Langlais <philippe.langlais@linaro.org>
2012-01-10 17:47:30 +0100

    mach-ux500: Configure SysClkReqRfClkBuf
    Added the possibility to configure SysCl1kReqRfClkBuf - SysClk8ReqRfClkBuf
    
    Signed-off-by: Kennet Wallden <kennet.wallden@stericsson.com>
    Signed-off-by: Philippe Langlais <philippe.langlais@linaro.org>

--- arch/arm/mach-ux500/board-mop500.c
+++ arch/arm/mach-ux500/board-mop500.c
@@ -41,6 +41,7 @@
 #include <linux/pwm_backlight.h>
 #include <linux/gpio/nomadik.h>
 #include <linux/leds.h>
+#include <linux/mfd/abx500/ab8500-sysctrl.h>
 
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
@@ -146,6 +147,16 @@ static struct ab8500_gpio_platform_data ab8500_gpio_pdata = {
 	 * GPIO pullup/pulldown configuration.
 	 */
 	.config_pullups    = {0xE0, 0x01, 0x00, 0x00, 0x00, 0x00},
+};
+
+static struct ab8500_sysctrl_platform_data ab8500_sysctrl_pdata = {
+	/*
+	 * SysClkReq1RfClkBuf - SysClkReq8RfClkBuf
+	 * The initial values should not be changed because of the way
+	 * the system works today
+	 */
+	.initial_req_buf_config
+			= {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
 };
 
 #ifdef CONFIG_INPUT_AB8500_ACCDET
@@ -270,6 +281,7 @@ static struct ab8500_platform_data ab8500_platdata = {
 	.fg		= &ab8500_fg_plat_data,
 	.chargalg	= &ab8500_chargalg_plat_data,
 	.gpio		= &ab8500_gpio_pdata,
+	.sysctrl 	= &ab8500_sysctrl_pdata,
 #ifdef CONFIG_INPUT_AB8500_ACCDET
 	.accdet = &ab8500_accdet_pdata,
 #endif
