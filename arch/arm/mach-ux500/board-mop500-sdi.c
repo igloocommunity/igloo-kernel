ce4b59b8d57a4ccf8eebe18d17e330cddb3e050e
Philippe Langlais <philippe.langlais@stericsson.com>
2012-01-19 14:50:19 +0100

    mach-ux500: sdi: Fixing sdi0 access on snowball
    This patch fix the SD/MMC boot problem on 3.2 (Bug #912302)
    MMC_CAP_SD_HIGHSPEED is not supported on snowball resulting on
    initialisation errors.
    There is no sdi2 on Snowball too.
    
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Fredrik Soderstedt <fredrik.soderstedt@stericsson.com>
    Signed-off-by: Philippe Langlais <philippe.langlais@linaro.org>

--- arch/arm/mach-ux500/board-mop500-sdi.c
+++ arch/arm/mach-ux500/board-mop500-sdi.c
@@ -279,7 +279,8 @@ void __init snowball_sdi_init(void)
 {
 	u32 periphid = 0x10480180;
 
-	mop500_sdi2_data.capabilities |= MMC_CAP_MMC_HIGHSPEED;
+	/* On Snowball MMC_CAP_SD_HIGHSPEED isn't supported (Hardware issue?) */
+	mop500_sdi0_data.capabilities &= ~MMC_CAP_SD_HIGHSPEED;
 
 	/* On-board eMMC */
 	db8500_add_sdi4(&mop500_sdi4_data, periphid);
