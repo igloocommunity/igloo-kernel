693e5b2ab3d7b46d891740b26c75139bf5cec4e4
Philippe Langlais <philippe.langlais@linaro.org>
2012-03-08 13:21:36 +0100

    mfd: ab8500-i2c: Include the right prcmu header file
    Signed-off-by: Philippe Langlais <philippe.langlais@linaro.org>

--- drivers/mfd/ab8500-i2c.c
+++ drivers/mfd/ab8500-i2c.c
@@ -11,7 +11,7 @@
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/mfd/abx500/ab8500.h>
-#include <linux/mfd/db8500-prcmu.h>
+#include <linux/mfd/dbx500-prcmu.h>
 
 
 static int ab8500_i2c_write(struct ab8500 *ab8500, u16 addr, u8 data)
